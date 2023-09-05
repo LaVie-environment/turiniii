d8bf3106355600ae73a39bcac5c8d21ed3b58a52
armzig <bellotaophyc@outlook.com>
2023-09-05 06:29:45 +0100

    modify the security group

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -60,6 +60,14 @@ resource "aws_security_group" "uat-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+   # Allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
 
 data "aws_vpc" "default" {
